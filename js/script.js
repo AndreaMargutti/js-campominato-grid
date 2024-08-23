cacf3cdc90686fb18573544e2f91135fc25c00d9
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-23 16:05:06 +0200

    create cell with js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('JS OK');
+// # Fase di preparazione
+//recupero gli elementi dal DOM
+const playBtn = document.getElementById('play');
+const grid = document.getElementById('grid');
+
+// # Funzioni
+//funzione per creare una cella
+function createCell () {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    return cell;
+}
+
+//funzione per generare un numero casuale da 1 a 100
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+//generare le celle al click del bottone
+playBtn.addEventListener('click', function() {
+    for(let i = 0; i < 100; i++) {
+        //genero cento celle
+        const cell = createCell();
+    }
+})
