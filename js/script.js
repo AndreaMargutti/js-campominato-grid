0beeb51c5e2f3857fa091e99a3f5a566dbcd3e85
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-26 16:30:44 +0200

    added score

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const form = document.querySelector('form');
 const difficulty = document.querySelector('select');
 const playBtn = document.getElementById('play');
 const grid = document.getElementById('grid');
+const scoreCounter = document.getElementById('score');
 
 // # Funzioni
 //funzione per creare una cella
@@ -16,41 +17,53 @@ function createCell () {
 //funzione di inizio giocp
 function startGame (event) {
     event.preventDefault();
+
+    
     //# Fase di elaborazione
     let rows = 10;
     let cols = 10;
-
+    let score = 0;
+    
     //cambio alla difficoltà
     const level = difficulty.value;
     
     switch(level) {
-        case 'normal':
-            rows = 9
-            cols = 9
-            break;
+    case 'normal':
+        rows = 9
+        cols = 9
+        break;
         case 'hard':
-            rows = 7
-            cols = 7
-            break;
+        rows = 7
+        cols = 7
+        break;
     }
             
     const totalCells = rows * cols
-
+            
     //metto la classe corrispondente alla girglia
-        //metto la classe giusta alla griglia
-        grid.classList.remove('hard', 'normal', 'easy');
-        grid.classList.add(level);
-
+    //metto la classe giusta alla griglia
+    grid.classList.remove('hard', 'normal', 'easy');
+    grid.classList.add(level);
+        
     //svuoto la griglia
     grid.innerHTML = '';
+    //cambia il bottone in 'ricomincia'
+    playBtn.innerText = 'ricomincia'
+
     for(let i = 0; i < totalCells; i++) {
         //genero cento celle
         const cell = createCell();
         
         //colorariamo la cella al click e facciamo un log del suo numero
         cell.addEventListener('click', function() {
-            cell.classList.toggle('clicked');
-            console.log(cellNumber)
+            if(!cell.classList.contains('clicked')){
+                cell.classList.add('clicked');
+                console.log(cellNumber)
+                score = ++score;
+                scoreCounter.innerText = score;
+            } else {
+                return;
+            }
         })
     
         //inserire il numero nella cella
@@ -63,4 +76,5 @@ function startGame (event) {
     }
 }    
 
+//chiamo la funzione ed eseguo tutto il programma
 form.addEventListener('submit', startGame);
